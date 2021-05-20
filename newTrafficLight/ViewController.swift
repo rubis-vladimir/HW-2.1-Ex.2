9a1a564d895fd19b75076f56a3ab3c868bf375ea
rubis-vladimir <rubis-vladimir>
2021-05-20 15:02:32 +0300

    Свежая версия Светофора

--- newTrafficLight/ViewController.swift
+++ newTrafficLight/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
-
+enum CurrentLight {
+    case red, yellow, green
+}
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet var allLight: [UIView]!
+   
+    var currentLight = CurrentLight.red
+    let isOn: CGFloat = 1.0
+    let isOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for index in 0..<allLight.count {
+        allLight[index].layer.masksToBounds = true
+        allLight[index].layer.cornerRadius =
+        allLight[index].frame.width/2
+        }
+        
     }
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = isOn
+            yellowLight.alpha = isOff
+            greenLight.alpha = isOff
+            currentLight = .yellow
+            
+        case .yellow:
+            redLight.alpha = isOff
+            yellowLight.alpha = isOn
+            greenLight.alpha = isOff
+            currentLight = .green
+        case .green:
+            redLight.alpha = isOff
+            yellowLight.alpha = isOff
+            greenLight.alpha = isOn
+            currentLight = .red
+        }
+    }
+    
+    
+    
+    
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        super.viewWillTransition(to: size, with: coordinator)
+//        for index in 0..<allLight.count {
+//        allLight[index].layer.masksToBounds = true
+//        allLight[index].layer.cornerRadius =
+//        allLight[index].frame.width/2
+//        }
+//    }
 
 }
 
